f5f57bd8645a85a848246ad6d7aca497ba6bb168
Michaël Trazzi <mtrazzi@student.42.fr>
2018-02-03 17:35:17 +0100

    Final 5x5 maps. Changed name of levels.

--- src/maps.js
+++ src/maps.js
@@ -9,22 +9,8 @@ const levels = [
 		'g', 'g', 'g', 'g', 'g',
 		],
 		solution: {
-			message: 'Stifle the flames...',
+			message: 'Can you stop it?',
 			none: 'f'
-		}
-	},
-	{
-		//EASY 2 : Build a forest
-		startBoard: [
-		'w', 'g', 'g', 'g', 'g',
-		'w', 'g', 'g', 'g', 'g',
-		'w', 'w', 'w', 'w', 'w',
-		'w', 'g', 'g', 'g', 'g',
-		'g', 'g', 'g', 'g', 'g',
-		],
-		solution: {
-			message: 'Build a forest...',
-			all: 'g'
 		}
 	},
 	{
@@ -37,78 +23,124 @@ const levels = [
 		'g', 'g', 'g', 'g', 'g',
 		],
 		solution: {
-			message: 'Burn the trees...',
+			message: 'What could go wrong?',
 			none: 'g'
-		}
-	},
-	{
-		//EASY 4 : Extinguish fire
-		startBoard: [
-		'f', 'f', 'f', 'f', 'w',
-		'g', 'g', 'g', 'g', 'f',
-		'f', 'f', 'f', 'f', 'f',
-		'g', 'g', 'g', 'g', 'f',
-		'f', 'f', 'f', 'f', 'f',
-		],
-		solution: {
-			message: 'Extinguish fire...',
-			none: 'f'
-		}
-	},
-	{
-		//EASY 5 : Inondation
-		startBoard: [
-		'w', 'w', 'w', 'w', 'w',
-		'g', 'g', 'g', 'g', 'g',
-		'g', 'g', 'g', 'g', 'g',
-		'g', 'g', 'g', 'g', 'g',
-		'f', 'f', 'f', 'f', 'f',
-		],
-		solution: {
-			message: 'Inondation: only water left',
-			all: 'w'
 		}
 	},
 	{
 		//EASY 6 : Build the forest
 		startBoard: [
-		'w', 'f', 'f', 'f', 'f',
-		'f', 'f', 'f', 'f', 'f',
-		'f', 'f', 'f', 'f', 'f',
-		'f', 'f', 'f', 'f', 'f',
-		'f', 'f', 'f', 'f', 'g',
+		'w', 'f', 'f',
+		'f', 'f', 'f',
+		'f', 'f', 'g',
 		],
 		solution: {
-			message: 'Build the forest: all trees',
+			message: 'Mother nature',
 			all: 'g'
 		}
 	},
 	{
-		//EASY 7 : Burn Everything
+		//EASY 9 : Inondation
 		startBoard: [
-		'f', 'w', 'w', 'w', 'w',
-		'f', 'w', 'w', 'w', 'w',
-		'f', 'w', 'w', 'w', 'w',
-		'f', 'w', 'w', 'w', 'w',
+		'w', 'w', 'w', 'w', 'w',
+		'w', 'g', 'g', 'g', 'g',
 		'f', 'g', 'g', 'g', 'g',
+		'w', 'g', 'g', 'g', 'g',
+		'w', 'f', 'f', 'f', 'f',
 		],
 		solution: {
-			message: 'Burn Everything: all fire',
-			all: 'f'
+			message: 'Tsunami',
+			all: 'w'}
+	},
+	{
+		//EASY 10 : Burn Everything
+		startBoard: [
+		'f', 'w', 'w', 'w', 'f',
+		'f', 'f', 'f', 'f', 'f',
+		'f', 'f', 'f', 'f', 'f',
+		'f', 'f', 'f', 'f', 'f',
+		'g', 'w', 'w', 'w', 'w',
+		],
+		solution: {
+			message: 'Some people just want to watch the world burn',
+			all: 'f'}
+	},
+	{
+		//EASY 11 : Only trees
+		startBoard: [
+		'w', 'f', 'f', 'f', 'g',
+		'w', 'g', 'g', 'g', 'f',
+		'w', 'w', 'w', 'w', 'w',
+		'w', 'w', 'w', 'w', 'f',
+		'w', 'w', 'w', 'w', 'w',
+		],
+		solution: {
+			message: 'Save the amazon forest',
+			all: 'g'}
+	},
+	{
+		//EASY 12 : Inondation
+		startBoard: [
+		'w', 'g', 'g', 'g', 'f',
+		'w', 'w', 'w', 'w', 'g',
+		'g', 'g', 'g', 'w', 'g',
+		'f', 'f', 'g', 'w', 'g',
+		'w', 'f', 'g', 'w', 'g',
+		],
+		solution: {
+			message: 'Join the rivers they said',
+			all: 'w'}
+	},
+	{
+		//EASY 15 : Burn everything
+		startBoard: [
+		'w', 'f', 'f',
+		'f', 'f', 'f',
+		'f', 'f', 'g',
+		],
+		solution: {
+			message: 'Would you like a glass of water',
+			all: 'w'
 		}
 	},
 	{
-		//EASY 8 : Burn Everything
+		//EASY 13 : Build the Forest
 		startBoard: [
+		'w', 'f', 'w', 'f', 'w',
 		'f', 'g', 'g', 'g', 'f',
-		'g', 'w', 'w', 'w', 'g',
-		'f', 'f', 'f', 'f', 'f',
-		'g', 'w', 'w', 'w', 'g',
+		'w', 'g', 'g', 'g', 'w',
 		'f', 'g', 'g', 'g', 'f',
+		'g', 'w', 'w', 'f', 'w',
 		],
 		solution: {
-			message: 'Burn Everything: all fire',
-			all: 'f'		}
+			message: 'For the jungle',
+			all: 'g'}
+	},
+	{
+		//EASY 14 : Build Forest
+		startBoard: [
+		'g', 'f', 'f', 'f', 'w',
+		'f', 'g', 'f', 'g', 'f',
+		'f', 'g', 'g', 'g', 'f',
+		'f', 'g', 'f', 'g', 'f',
+		'g', 'f', 'f', 'f', 'w',
+		],
+		solution: {
+			message: 'What could go green ?',
+			all: 'g'}
+	},
+
+	{
+		//EASY 16 : Build Forest
+		startBoard: [
+		'w', 'f', 'f',
+		'f', 'f', 'f',
+		'f', 'f', 'g',
+		],
+		solution: {
+			message: 'Fire, fire everywhere',
+			all: 'f'
+		}
 	},
 ]
 
