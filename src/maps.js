0b07f43a55fe58ae498b2815cf9143e5317129c6
Michaël Trazzi <mtrazzi@student.42.fr>
2018-02-03 16:00:50 +0100

    Added more maps (Now 8 maps)

--- src/maps.js
+++ src/maps.js
@@ -1,8 +1,6 @@
 const levels = [
 	{
-		//TUTORIEL
-		//////////
-		//ETAPE 1 : Stifle the flames
+		//EASY 1 : Stifle the flames
 		startBoard: [
 		'w', 'g', 'g', 'g', 'g',
 		'w', 'g', 'g', 'g', 'g',
@@ -16,9 +14,7 @@ const levels = [
 		}
 	},
 	{
-		//TUTORIEL
-		//////////
-		//ETAPE 2 : Build a forest
+		//EASY 2 : Build a forest
 		startBoard: [
 		'w', 'g', 'g', 'g', 'g',
 		'w', 'g', 'g', 'g', 'g',
@@ -32,7 +28,7 @@ const levels = [
 		}
 	},
 	{
-		//EASY 1 : Burn the trees
+		//EASY 3 : Burn the trees
 		startBoard: [
 		'f', 'w', 'g', 'g', 'g',
 		'g', 'w', 'g', 'w', 'g',
@@ -44,6 +40,75 @@ const levels = [
 			message: 'Burn the trees...',
 			none: 'g'
 		}
+	},
+	{
+		//EASY 4 : Extinguish fire
+		startBoard: [
+		'f', 'f', 'f', 'f', 'w',
+		'g', 'g', 'g', 'g', 'f',
+		'f', 'f', 'f', 'f', 'f',
+		'g', 'g', 'g', 'g', 'f',
+		'f', 'f', 'f', 'f', 'f',
+		],
+		solution: {
+			message: 'Extinguish fire...',
+			none: 'f'
+		}
+	},
+	{
+		//EASY 5 : Inondation
+		startBoard: [
+		'w', 'w', 'w', 'w', 'w',
+		'g', 'g', 'g', 'g', 'g',
+		'g', 'g', 'g', 'g', 'g',
+		'g', 'g', 'g', 'g', 'g',
+		'f', 'f', 'f', 'f', 'f',
+		],
+		solution: {
+			message: 'Inondation: only water left',
+			all: 'w'
+		}
+	},
+	{
+		//EASY 6 : Build the forest
+		startBoard: [
+		'w', 'f', 'f', 'f', 'f',
+		'f', 'f', 'f', 'f', 'f',
+		'f', 'f', 'f', 'f', 'f',
+		'f', 'f', 'f', 'f', 'f',
+		'f', 'f', 'f', 'f', 'g',
+		],
+		solution: {
+			message: 'Build the forest: all trees',
+			all: 'g'
+		}
+	},
+	{
+		//EASY 7 : Burn Everything
+		startBoard: [
+		'f', 'w', 'w', 'w', 'w',
+		'f', 'w', 'w', 'w', 'w',
+		'f', 'w', 'w', 'w', 'w',
+		'f', 'w', 'w', 'w', 'w',
+		'f', 'g', 'g', 'g', 'g',
+		],
+		solution: {
+			message: 'Burn Everything: all fire',
+			all: 'f'
+		}
+	},
+	{
+		//EASY 8 : Burn Everything
+		startBoard: [
+		'f', 'g', 'g', 'g', 'f',
+		'g', 'w', 'w', 'w', 'g',
+		'f', 'f', 'f', 'f', 'f',
+		'g', 'w', 'w', 'w', 'g',
+		'f', 'g', 'g', 'g', 'f',
+		],
+		solution: {
+			message: 'Burn Everything: all fire',
+			all: 'f'		}
 	},
 ]
 
