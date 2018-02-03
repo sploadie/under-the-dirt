6450360d476e554e0f07c34f7f9295636543f9a9
Antonin RIDOLFI <aridolfi@student.42.fr>
2018-02-03 16:17:40 +0100

    Quick Menu

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import { Header, Icon } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
 const Home = () => (
+
   <div>
-    <h1>Hello World</h1>
+
+    <Header className="game-title" as='h1'>Under the Dirt</Header>
+
+    <div className="menu-link-container">
+
+      <Header className="menu-link" as='h2' textAlign='center'>
+
+        <Link to="/game">Play</Link>
+
+      </Header>
+
+      <Header className="menu-link" as='h2' textAlign='center'>
+
+        <Link to="/scoreboard">Scoreboard</Link>
+
+      </Header>
+
+      <Header className="menu-link" as='h2' textAlign='center'>
+
+        <Link to="/credits">Credits</Link>
+
+      </Header>
+
+    </div>
+
   </div>
+
 )
 
 export default Home
--- src/logic.js
+++ src/logic.js
@@ -1,57 +1,42 @@
 "use strict";
 
 const directionIncrement = {
-	up: (pos, side) => pos - side,
-	down: (pos, side) => pos + side,
-	left: (pos, side) => pos - 1,
-	right: (pos, side) => pos + 1,
+  up: (pos, side) => pos - side,
+  down: (pos, side) => pos + side,
+  left: (pos, side) => pos - 1,
+  right: (pos, side) => pos + 1,
 }
 
 function doMove(boardState, move) {
 
-	const gridSize = Math.sqrt(boardState.length);
-	if (gridSize !== Math.floor(gridSize)) {
-		console.error(`Board has size ${boardState.length}??`)
-	}
+  const gridSize = Math.sqrt(boardState.length);
+  if (gridSize !== Math.floor(gridSize)) {
+  console.error(`Board has size ${boardState.length}??`)
+  }
 
-	if (move.pos >= boardState.length) {
-		console.error("[UNKNOWN ACTION]");
-		return boardState;
-	}
+  if (move.pos >= boardState.length) {
+    console.error("[UNKNOWN ACTION]");
+    return boardState;
+  }
 
-	const newBoard = boardState.slice();
-	let pos = move.pos;
+  const newBoard = boardState.slice();
+  let pos = move.pos;
 
-	const eats = {
-		w: 'f',
-		g: 'w',
-		f: 'g',
-	}
+  const eats = {
+    w: 'f',
+    g: 'w',
+    f: 'g',
+  }
 
-	for (let i = 1; i < gridSize; i++) {
-		pos = directionIncrement[move.direction](pos, gridSize);
-		console.log('Pos:', pos);
-		if (newBoard[pos] != eats[move.element]) {
-			break;
-		}
-		newBoard[pos] = move.element;
-	}
+  for (let i = 1; i < gridSize; i++) {
+    pos = directionIncrement[move.direction](pos, gridSize);
+    if (newBoard[pos] != eats[move.element]) {
+      break;
+    }
+    newBoard[pos] = move.element;
+  }
 
-	return newBoard;
+  return newBoard;
 }
-
-// const boardState = [
-// 	'w', 'w', 'w',
-// 	'w', 'w', 'g',
-// 	'w', 'f', 'f'
-// ];
-//
-// const move = {
-// 	element: 'w',
-// 	pos: 1,
-// 	direction: 'down'
-// }
-//
-// console.log(doMove(boardState, move))
 
 export default doMove
